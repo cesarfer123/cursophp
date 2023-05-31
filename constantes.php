8742dc9572accf8bffc4b54c0391c164d7b76680
cesar <fernandocheroayasta@outlook.com>
2023-05-31 17:02:45 -0500

    constantes

---
+++ constantes.php
@@ -0,0 +1,22 @@
+<?php
+
+// DEFINICION ANTIGUA
+
+/** 
+define('NOMBRE','carlos');
+
+define('NOMBRE',7);
+
+echo NOMBRE;
+*/
+
+// DEFINICION NUEVA
+
+const NOMBRE='ashlie';
+
+echo NOMBRE .'</br>';
+
+define('ALUMNOS',array('cesar','percy','manuel'));
+
+echo ALUMNOS[0];
+
