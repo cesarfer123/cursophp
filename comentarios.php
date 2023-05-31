f0ee7f3530d11d48e791c3dbd13a40b57d35acb0
cesar <fernandocheroayasta@outlook.com>
2023-05-31 16:18:27 -0500

    agregando comentarios

---
+++ comentarios.php
@@ -0,0 +1,14 @@
+<?php
+
+    // mi primer comentario
+    echo "holamundo </br>";
+
+    #mi segundo comentario
+    echo "hola mundo 2 </br>";
+
+    /*
+    comentarios
+    en multiples lineas
+    */
+    echo "hola mundo 3";
+
