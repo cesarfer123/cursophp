c32f49bdba48c0345714b17e0632766a890a94a2
cesar <fernandocheroayasta@outlook.com>
2023-05-31 16:25:10 -0500

    tipo de variables

---
+++ tipodedatos.php
@@ -0,0 +1,9 @@
+<?php
+// REPRESENTADO POR 1
+echo true . '</br>';
+// REPRESENTADO POR 0
+echo FALSE . '</br>';
+// NUMERO ENTERO
+echo var_dump(18) . '</br>';
+// var_dump sirve para ver el tipo de dato de la variable o valor.
+echo  var_dump('hola mundo');
