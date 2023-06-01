bab19ebc283f932d143304f7eabe00b968afecc8
cesar <fernandocheroayasta@outlook.com>
2023-06-01 14:52:38 -0500

    operadores

---
+++ asignacion.php
@@ -0,0 +1,14 @@
+<?php
+// operadores de asignacion
+$numero=5;
+// $numero+=5;
+// $numero-=2;
+// $numero*=2;
+// $numero/=2;
+$texto="essto es un texto de prueba ";
+// $texto.=100;
+$texto.="otro texto";
+
+
+// echo $numero;
+echo $texto;
---
+++ operadores.php
@@ -0,0 +1,12 @@
+<?php
+
+$num1=5;
+$num2=2;
+
+$resultado=7-4+($num1*$num2)/2;
+echo $resultado;
+
+// exponenciacion
+// usar **
+// residuo
+// usar %
